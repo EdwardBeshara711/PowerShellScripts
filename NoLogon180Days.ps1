fb71e8fd3d93c43932120c96bbd70f2de4fc8da6
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-20 15:01:10 -0600

    making it better

--- NoLogon180Days.ps1
+++ NoLogon180Days.ps1
@@ -3,11 +3,31 @@
 
 $ou = "OU=End_Users,DC=flatironcorp,DC=com"
 
-$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname,AccountExpirationdate,PasswordLastSet,LastLogon  | 
+$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,
+    whenChanged,
+    lastLogonDate,
+    displayname,
+    whenCreated,
+    distinguishedname,
+    AccountExpirationdate,
+    PasswordLastSet,
+    LastLogonTimestamp,
+    Manager  | 
+    
     Where-Object {
         ($_.lastLogonDate -lt (Get-Date).AddDays(-180)) -and
         ($_.whenCreated -lt (Get-Date).AddDays(-180)) -and
         ($_.PasswordLastSet -lt (Get-Date).AddDays(-180)) } | 
-    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname,AccountExpirationdate,PasswordLastSet,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}} 
+    
+    Select-Object SamAccountName,
+    whenChanged,
+    lastLogonDate,
+    displayname,
+    whenCreated,
+    distinguishedname,
+    AccountExpirationdate,
+    PasswordLastSet,
+    @{n='LastLogonTimestamp';e={[DateTime]::FromFileTime($_.LastLogonTimestamp)}},
+    @{N='Manager';E={(Get-ADUser $_.Manager).sAMAccountName}}
 
 

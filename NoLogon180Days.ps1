2327d0902365ca24bddac661550fa85717678f7f
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-20 14:27:10 -0600

    adding attribute

--- NoLogon180Days.ps1
+++ NoLogon180Days.ps1
@@ -3,10 +3,10 @@
 
 $ou = "OU=End_Users,DC=flatironcorp,DC=com"
 
-$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname | 
+$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname,AccountExpirationdate | 
     Where-Object {
         ($_.lastLogonDate -lt (Get-Date).AddDays(-180)) -and
         ($_.whenCreated -lt (Get-Date).AddDays(-180)) } | 
-    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname
+    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname,AccountExpirationdate
 
 

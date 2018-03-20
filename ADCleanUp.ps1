5497655f8e22b3d308dbc662c95b440ae3a95b9a
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-20 12:10:01 -0600

    getting users

---
+++ ADCleanUp.ps1
@@ -0,0 +1,7 @@
+$ou = "OU=End_Users,DC=flatironcorp,DC=com"
+
+$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated | 
+    Where-Object {
+        ($_.lastLogonDate -lt (Get-Date).AddDays(-180)) -and
+        ($_.whenCreated -lt (Get-Date).AddDays(-180))  } | 
+    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated

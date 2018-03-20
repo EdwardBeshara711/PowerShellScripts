41568d01301abb513a51e2dd8ce062d6b66e49b5
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-20 13:55:08 -0600

    just converting to simple script

--- ADCleanUp.ps1
+++ ADCleanUp.ps1
@@ -1,7 +1,12 @@
+
+#End Users - getting list of users
+
 $ou = "OU=End_Users,DC=flatironcorp,DC=com"
 
-$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated | 
+$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname | 
     Where-Object {
         ($_.lastLogonDate -lt (Get-Date).AddDays(-180)) -and
-        ($_.whenCreated -lt (Get-Date).AddDays(-180))  } | 
-    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated
+        ($_.whenCreated -lt (Get-Date).AddDays(-180)) } | 
+    Select-Object SamAccountName,whenChanged,lastLogonDate,displayname,whenCreated,distinguishedname
+
+

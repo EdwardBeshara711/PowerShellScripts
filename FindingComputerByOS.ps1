23296c4b667d2c197d8c327d7a52d929ad70ccf5
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-23 10:01:30 -0600

    simple script add

---
+++ FindingComputerByOS.ps1
@@ -0,0 +1,8 @@
+#finding computer with certain name
+
+$name = "Windows*server*"
+
+GET-ADComputer -Filter { OperatingSystem -Like $name } -Properties OperatingSystem,description |
+Select Name,OperatingSystem,description |
+sort-object -property name | ft
+

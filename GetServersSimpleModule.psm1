2fd6b1033c1bade6313267991a7372ffc3d81321
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-23 10:07:17 -0600

    simple module to get systems by operating system

---
+++ GetServersSimpleModule.psm1
@@ -0,0 +1,57 @@
+# GET-ADComputer -Filter { OperatingSystem -Like 'Windows*Server*' } -Properties Name,OperatingSystem,description |
+# Select Name,OperatingSystem, description | sort-object -property name,OperatingSystem, description | sort-object -property name | Out-GridView
+
+
+function Get-Servers {
+
+        [CmdletBinding()]
+
+        Param (
+                                   
+        )
+
+        BEGIN {}
+
+        PROCESS {
+            
+            $filter = @{
+                Filter = "OperatingSystem -Like 'Windows*Server*'"
+            }
+
+            $serverList = GET-ADComputer @filter -Properties Name,OperatingSystem,description | Select-Object Name,OperatingSystem,description | sort name
+
+            foreach ($server in $serverList) {
+
+                try {
+                    
+
+                    $properties = @{
+                        ServerName = $server.name
+                        OperatingSystem = ($server.OperatingSystem).Replace("Windows Server","")
+                        Description = $server.description
+                    }
+                    
+                }
+                catch {
+                    
+                    $properties = @{
+                        ServerName = $null
+                        OperatingSystem = $null
+                        Description = $null
+                    }
+                    
+                }
+
+                finally {
+                    $object = New-Object -TypeName psobject -Property $properties
+                    Write-Output $object
+                }
+
+                
+            }
+
+        }
+
+        END {}
+    }
+

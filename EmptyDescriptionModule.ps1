222c7c8039eeadfd5dc7ce7107ef6c4d126fb8ec
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-23 10:12:33 -0600

    adding module
    module finds empty descriptions in ad using ldap filter

---
+++ EmptyDescriptionModule.ps1
@@ -0,0 +1,47 @@
+function Get-EmptyDescriptionInfo {
+
+        [CmdletBinding()]
+
+        Param (
+            [Parameter(
+                Mandatory=$true,
+                ValueFromPipeline=$true,
+                ValueFromPipelineByPropertyName=$true,
+                HelpMessage="OU disstinguished name"
+                )]
+            [string]$OrganizationalUnit
+        )
+
+        BEGIN {
+            Write-Verbose "begin"
+        }
+
+        PROCESS {
+            foreach ($account in $Accounts) {
+                try {
+                    Write-Verbose "try"
+                    $filter = "(&(!description=*))"
+                    $useraccount = Get-ADUser -Searchbase $OrganizationalUnit -LDAPFilter $filter -Properties name
+                    $properties = @{
+                        Account = $account
+                        Descriptioin = $useraccount.description
+                    }
+                }
+                catch {
+                    $properties = @{
+                        Account = $null
+                        Descriptioin = $nulle
+                    
+                    }
+                }
+                finally {
+                    $object = New-Object -TypeName psobject -Property $properties
+                    Write-Output $object
+                }
+            }
+        }
+
+        END {}
+    }
+
+Get-EmptyDescriptionInfo

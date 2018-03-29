d524522c69e3f9cb63f3e10bf796281f38b42ddc
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-29 13:00:34 -0600

    create script

---
+++ NoLogonUsers180days.ps1
@@ -0,0 +1,31 @@
+#End Users - getting list of users
+
+$ou = "OU=End_Users,DC=contoso,DC=com"
+
+$Users = Get-AdUser -filter {Enabled -eq $TRUE} -Searchbase $ou  -Properties SamAccountName,
+    whenChanged,
+    lastLogonDate,
+    displayname,
+    whenCreated,
+    distinguishedname,
+    AccountExpirationdate,
+    PasswordLastSet,
+    LastLogonTimestamp,
+    Manager  | 
+    
+    Where-Object {
+        ($_.lastLogonDate -lt (Get-Date).AddDays(-180)) -and
+        ($_.whenCreated -lt (Get-Date).AddDays(-180)) -and
+        ($_.PasswordLastSet -lt (Get-Date).AddDays(-180)) } | 
+    
+    Select-Object SamAccountName,
+    whenChanged,
+    lastLogonDate,
+    displayname,
+    whenCreated,
+    distinguishedname,
+    AccountExpirationdate,
+    PasswordLastSet,
+    @{n='LastLogonTimestamp';e={[DateTime]::FromFileTime($_.LastLogonTimestamp)}},
+    @{N='Manager';E={(Get-ADUser $_.Manager).sAMAccountName}}
+

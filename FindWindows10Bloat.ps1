08df0306fc9f6c10e2e704af31f22238a53b5e6c
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2018-03-29 13:02:44 -0600

    formatting

--- FindWindows10Bloat.ps1
+++ FindWindows10Bloat.ps1
@@ -1,6 +1,9 @@
-# get windows 10 bloat
+# get windows 10 bloat list
 
-Get-AppxPackage -AllUsers | where-object {($_.name –notlike “*store*”) -and ($_.name –notlike "*.NET.Native*")} | select-object name | ft
+Get-AppxPackage -AllUsers |
+  where-object {($_.name –notlike “*store*”) -and ($_.name –notlike "*.NET.Native*")} |
+  select-object name |
+  ft
 
 
 
